e7b60b714e504ca728c639cf69812daa77f2fc04
Dev <83958063+DevSTheDeveloper@users.noreply.github.com>
2025-04-01 19:16:44 +0100

    Update SpeedControl.java

--- src/EV3/SpeedControl.java
+++ src/EV3/SpeedControl.java
@@ -5,7 +5,7 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.Color;
 
 public class SpeedControl implements Runnable {
-    private final MotorControlBehavior motorControlBehavior;
+    private final MotorControlBehavior motorControlBehavior; 
     private final ColorDetectionBehavior colorDetectionBehavior;
     private boolean suppressed = false;
     private int previousSpeed = null;
@@ -21,131 +21,7 @@ public class SpeedControl implements Runnable {
 
     private synchronized void action() {
         suppressed = false;
-        int blueSpeed = 150;
-
-        while (!suppressed) {
-            String detectedColor = colorDetectionBehavior.getDetectedColor(); // Get current detected color
-
-            if (detectedColor.equals("BLUE")) {
-                if (previousSpeed == null) { // store previous speed only the first time
-                    previousSpeed = motorControlBehavior.getSpeed();
-                }
-                motorControlBehavior.setSpeed(blueSpeed);
-            } else if (previousSpeed != null) {
-                motorControlBehavior.setSpeed(previousSpeed);
-                previousSpeed = null; // reset previousSpeed when reverting
-            }
-
-            try {
-                Thread.sleep(50); // cpu help
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    public void suppress() {
-        suppressed = true;
-    }
-
-    public void run() {
-        while (true) {
-            if (takeControl()) {
-                action();
-            }
-            try {
-                Thread.sleep(50); // cooldown let other tasks run
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-}package EV3;
-//By Faris
-
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.Color;
-
-public class SpeedControl implements Runnable {
-    private final MotorControlBehavior motorControlBehavior;
-    private final ColorDetectionBehavior colorDetectionBehavior;
-    private boolean suppressed = false;
-    private int previousSpeed = null;
-
-    public SpeedControl(MotorControlBehavior motorControlBehavior, ColorDetectionBehavior colorDetectionBehavior) {
-        this.motorControlBehavior = motorControlBehavior;
-        this.colorDetectionBehavior = colorDetectionBehavior;
-    }
-
-    private synchronized boolean takeControl() {
-        return colorDetectionBehavior.getDetectedColor().equals("BLUE");
-    }
-
-    private synchronized void action() {
-        suppressed = false;
-        int blueSpeed = 150;
-
-        while (!suppressed) {
-            String detectedColor = colorDetectionBehavior.getDetectedColor(); // Get current detected color
-
-            if (detectedColor.equals("BLUE")) {
-                if (previousSpeed == null) { // store previous speed only the first time
-                    previousSpeed = motorControlBehavior.getSpeed();
-                }
-                motorControlBehavior.setSpeed(blueSpeed);
-            } else if (previousSpeed != null) {
-                motorControlBehavior.setSpeed(previousSpeed);
-                previousSpeed = null; // reset previousSpeed when reverting
-            }
-
-            try {
-                Thread.sleep(50); // cpu help
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-
-    public void suppress() {
-        suppressed = true;
-    }
-
-    public void run() {
-        while (true) {
-            if (takeControl()) {
-                action();
-            }
-            try {
-                Thread.sleep(50); // cooldown let other tasks run
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-}package EV3;
-//By Faris
-
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.Color;
-
-public class SpeedControl implements Runnable {
-    private final MotorControlBehavior motorControlBehavior;
-    private final ColorDetectionBehavior colorDetectionBehavior;
-    private boolean suppressed = false;
-    private int previousSpeed = null;
-
-    public SpeedControl(MotorControlBehavior motorControlBehavior, ColorDetectionBehavior colorDetectionBehavior) {
-        this.motorControlBehavior = motorControlBehavior;
-        this.colorDetectionBehavior = colorDetectionBehavior;
-    }
-
-    private synchronized boolean takeControl() {
-        return colorDetectionBehavior.getDetectedColor().equals("BLUE");
-    }
-
-    private synchronized void action() {
-        suppressed = false;
-        int blueSpeed = 150;
+        int blueSpeed = 150; 
 
         while (!suppressed) {
             String detectedColor = colorDetectionBehavior.getDetectedColor(); // Get current detected color
